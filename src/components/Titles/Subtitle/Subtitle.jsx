8b1315be05bf42a68838baa2f02205a5ab92e20f
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 11:24:00 -0300

    criando componente de subtitulo

---
+++ src/components/Titles/Subtitle/Subtitle.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Subtitle = styled.h2`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+font-family: 'Montserrat', sans-serif;
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
+
+
+/* Gray 3 */
+
+color: #828282;
+`
+
+export default Subtitle
