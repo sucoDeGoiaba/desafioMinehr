66b44f72ffca970891a8814619f43ce24d665f43
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 11:56:08 -0300

    renomeando classe e ajustando margem

--- src/components/TitleSection/TitleSection.jsx
+++ src/components/TitleSection/TitleSection.jsx
@@ -5,7 +5,7 @@ import BtnFiltro from "../BtnFiltro/BtnFilter";
 import style from "./TitleSection.module.css"
 export default function TitleSection(){
     return(
-        <section className={style.topSec}>
+        <section className={style.topSection}>
             <div>
                 <Title>Dashboard</Title>
                 <Subtitle>Desafio Técnico Frontend</Subtitle>
