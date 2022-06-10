53d71eac63e351fa477621c9d97c9034224af7fb
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 11:36:07 -0300

    implementando section superior

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header/Header.jsx";
+import './App.css'
+import TitleSection from "./components/TitleSection/TitleSection.jsx";
 
 function App() {
   return (
     <>
       <Header/>
+      <main>
+        <TitleSection/>
+      </main>
     </>
   )
 }
---
+++ src/components/TitleSection/TitleSection.jsx
@@ -0,0 +1,17 @@
+import Title from "../Titles/Title/Title";
+import Subtitle from "../Titles/Subtitle/Subtitle";
+import BtnFiltro from "../BtnFiltro/BtnFilter";
+
+import style from "./TitleSection.module.css"
+export default function TitleSection(){
+    return(
+        <section className={style.topSec}>
+            <div>
+                <Title>Dashboard</Title>
+                <Subtitle>Desafio Técnico Frontend</Subtitle>
+            </div>
+            <BtnFiltro>Filtro</BtnFiltro>
+        </section>
+    )
+}
+
