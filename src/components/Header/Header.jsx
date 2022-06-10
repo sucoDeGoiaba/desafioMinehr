2bb391461e1413471c88964ecaae351165040b6e
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 09:57:49 -0300

    implementação do header e limpando projeto base

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+// export default function Header (){
+
+// }
+
+import styled from 'styled-components'
+
+const Header = styled.div`
+/* Rectangle 2 */
+
+position: absolute;
+width: 100%;
+height: 91px;
+left: 0px;
+top: 0px;
+
+/* Light purple */
+
+background: #CEC6CE;
+`
+export default Header
