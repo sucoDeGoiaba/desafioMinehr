e3c7aecd82a0d0f9a4ef282211df3ae91777fbd7
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 11:23:33 -0300

    criando componente de titulo

---
+++ src/components/Titles/Title/Title.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+/* Dashboard */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+
+font-family: 'Montserrat', sans-serif;
+font-style: normal;
+font-weight: 700;
+font-size: 28px;
+line-height: 34px;
+
+/* Dark purple */
+
+color: #5D405C;
+`
+
+export default Title
