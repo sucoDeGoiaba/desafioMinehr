562364620fb2d4bdef8808984789345598f40ae3
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 11:28:07 -0300

    criando componente de filtro

---
+++ src/components/BtnFiltro/BtnFilter.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const BtnFiltro = styled.button`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+width: 100px;
+height: 40px;
+
+/* Dark purple */
+background: #5D405C;
+
+/* Texto do botão */
+border-radius: 10px;
+font-family: 'Montserrat', sans-serif;
+font-style: normal;
+font-weight: 600;
+font-size: 14px;
+line-height: 17px;
+
+/* Gray */
+
+color: #F7F7F7;
+`
+
+export default BtnFiltro
