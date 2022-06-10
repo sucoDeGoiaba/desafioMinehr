49c6b0e4a7a32adc395680e876401c96c11c1bb4
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 15:44:00 -0300

    gerando gráfico de barras

---
+++ src/components/Graphics/BarsGraph.jsx
@@ -0,0 +1,56 @@
+import ReactApexChart from "react-apexcharts";
+import style from "./Graphics.module.css"
+export default function BarsGraph(){
+  const option = {
+    series: [{
+      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+    }],
+    options: {
+      title: {
+        text: 'Barras',
+        align: 'left',
+        margin: 0,
+        offsetX: 0,
+        offsetY: 0,
+        floating: false,
+        style: {
+          fontSize:  '16px',
+          fontWeight:  '700',
+          fontFamily:  'Montserrat',
+          color:  '#5D405C'
+        }
+    },
+      chart: {
+        type: 'bar',
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan', 'United States', 'China', 'Germany'
+        ],
+      }
+    },
+
+  }
+
+  return(
+    <div id="chart" className={style.container}>
+      <ReactApexChart 
+        className={style.graphic} 
+        options={option.options} 
+        series={option.series} 
+        type="bar" 
+        height={350} 
+        width={650}
+      />
+    </div>
+  )
+}
