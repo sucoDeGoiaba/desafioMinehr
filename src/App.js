d88d7039e397235063d66b7d918ed4cb63267855
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-10 18:20:19 -0300

    criando section pros graficos e os implementando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Header from "./components/Header/Header.jsx";
 import './App.css'
 import TitleSection from "./components/TitleSection/TitleSection.jsx";
-
+import BarsGraph from "./components/Graphics/BarsGraph.jsx";
+import ScatterGraph from "./components/Graphics/ScatterGraph.jsx";
+import GraphicSection from "./components/GraphicSection/GraphicSection.jsx";
 function App() {
   return (
     <>
       <Header/>
       <main>
         <TitleSection/>
+        <GraphicSection>
+          <BarsGraph/>
+          <ScatterGraph/>
+        </GraphicSection>
       </main>
     </>
   )
---
+++ src/components/GraphicSection/GraphicSection.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const GraphicSection = styled.section`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 25px;
+`
+
+export default GraphicSection
