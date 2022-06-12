4a58ab7f91bd68f334847ea52c8153405c59b093
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-11 23:07:34 -0300

    corrigindo propriedade

--- src/components/TitleSection/TitleSection.jsx
+++ src/components/TitleSection/TitleSection.jsx
@@ -2,7 +2,6 @@ import Title from "../Titles/Title/Title";
 import Subtitle from "../Titles/Subtitle/Subtitle";
 import BtnFiltro from "../BtnFiltro/BtnFilter";
 import funilIcon from "../../assets/funilIcon.svg"
-// import infoIcon from "../../assets/infoIcon.png"
 import style from "./TitleSection.module.css"
 export default function TitleSection(){
     return(
@@ -11,7 +10,7 @@ export default function TitleSection(){
                 <Title>Dashboard</Title>
                 <Subtitle>Desafio Técnico Frontend</Subtitle>
             </div>
-            <BtnFiltro><img src={funilIcon} alt="a" width='15px'/>Filtrar</BtnFiltro>
+            <BtnFiltro><img src={funilIcon} alt="Icone funil" width='15px'/>Filtrar</BtnFiltro>
         </section>
     )
 }
